2d5aeee0e18a455a687c46dd58b568243c99519f
AravaJayaram <86524159+AravaJayaram@users.noreply.github.com>
2021-06-26 19:36:04 +0530

    Add files via upload

---
+++ Info1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info1() {
+  return (
+    <div className="note">
+      <h1>Bootcamp on AI and ML</h1>
+      <p>A basic bootcamp on ML and AI</p>
+    </div>
+  );
+}
+export default Info1;
---
+++ Info2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info2() {
+  return (
+    <div className="note">
+      <h1>Bootcamp on full stack web development</h1>
+      <p>A basic bootcamp on web development</p>
+    </div>
+  );
+}
+export default Info2;
